a72dbceab13862db647a48a7cca09e0b206bb86d
Sayad Pervez <pervez2504@gmail.com>
2022-07-12 01:41:37 +0530

    basic parallax completed

--- parallax.js
+++ parallax.js
@@ -1,12 +1,8 @@
 let moon = document.getElementById("moon");
 let rocks = document.getElementById("rocks");
-let bg0 = document.getElementById("background0");
-let bg1 = document.getElementById("background1");
 
 window.addEventListener("scroll",()=>{
     let scrollY = window.scrollY;
-    bg0.style.top = scrollY*0.55 + "px";
-    bg1.style.top = scrollY*0.55 + "px";
-    moon.style.top = scrollY*0.85 + "px";
-    rocks.style.top = scrollY*0.65 + "px";
+    moon.style.top = scrollY*1.05 + "px";
+    rocks.style.top = scrollY*0.5 + "px";
 });
